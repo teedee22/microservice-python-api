d3d8d406e9245c6e32b5f16aff85dab97fd1f7ad
Toby <tdudleyoffice@gmail.com>
2020-11-09 11:12:09 +0000

    add system timestamps

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+from datetime import datetime
 import time
 
 from flask import Flask, jsonify, request, json
@@ -9,12 +10,17 @@ app = Flask(__name__)
 
 @app.route("/new", methods=['GET'])
 def new():
-    #data = request.json
+    timestart = datetime.now()
+
     number = random.randint(1,100) / 100
-    print(number)
+    print(f"Call will take {number} seconds")
     time.sleep(number)
-    payload = {"message": f"call took {number} seconds"}
+    timeend = datetime.now()
+
+    timetaken = round((timeend - timestart).total_seconds(), 3)
+    print(f"Call complete, system took {timetaken} to complete")
+    payload = {"message": f"call took {timetaken} seconds"}
     return jsonify(payload)
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5001, debug = True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug = True)
